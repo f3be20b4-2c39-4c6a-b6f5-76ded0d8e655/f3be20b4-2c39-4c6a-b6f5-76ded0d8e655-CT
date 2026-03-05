fee29491f4baffccae3737f7587dfaab2b601691
f3be20b4-2c39-4c6a-b6f5-76ded0d8e655 <erandix9@gmail.com>
2026-03-05 12:19:04 +0300

    Updates

--- LoaderPages/Connection.js
+++ LoaderPages/Connection.js
@@ -10,19 +10,19 @@ export const SPLASHSCREENPAGE=()=>{
 
     switch (NAMES) {
 
-        case "":MJOMBALISPLASH();
+        case "588b80c4-d0fc-4022-9cbf-19a4c9b7c13e":MJOMBALISPLASH();
             
             break;
 
-        case "":ASCOASPLASH();
+        case "ccfd169c-c80e-4d46-855b-7011d7d4e8d7":ASCOASPLASH();
             
             break;
 
-        case "":MAINSPLASH();
+        case "f3be20b4-2c39-4c6a-b6f5-76ded0d8e655":MAINSPLASH();
             
             break;
         
-        case "":QELSPLASH();
+        case "60f865a0-9713-4e1c-9a79-5a2e994ac97d":QELSPLASH();
             
             break;
     
