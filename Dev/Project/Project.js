8e65a6f8a8267ee9df922b59006a448b0755b5bd
f3be20b4-2c39-4c6a-b6f5-76ded0d8e655 <erandix9@gmail.com>
2026-03-17 18:44:44 +0300

    Updates

---
+++ Dev/Project/Project.js
@@ -0,0 +1,21 @@
+const ERANDES=()=>{
+
+    VIEWCONTROLLER();
+
+};
+
+const MOBILEVIEW=()=>{
+
+    AUTOSTART("","Movie Lander","Policies/MovieLander/MovieLander.txt","",()=>{
+
+    });
+
+};
+
+const DESKTOPVIEW=()=>{
+
+    AUTOSTART("","Movie Lander","Policies/MovieLander/MovieLander.txt","",()=>{
+
+    });
+
+};
